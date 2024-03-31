a3b4f5959023444b260bb8f7dde505e3935e4f9f
01fe22bca138 <01fe22bca138@kletech.ac.in>
2024-03-31 19:25:24 +0530

    Code for displaying employee details

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee{
+    String empId="138";
+    float salary=100000;
+
+    public void displayDetails()
+    {
+        System.out.println("Employee Id:"+empId);
+        System.out.println("Salary:"+salary);
+    }
+}
